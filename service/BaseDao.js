dd965da78c1a94b68cb56913c3e04fceeacff021
Diamond <diamondfsd@gmail.com>
2017-04-22 18:21:04 +0800

    remove header

--- service/BaseDao.js
+++ service/BaseDao.js
@@ -38,11 +38,7 @@ function get (path, data) {
 }
 function post (path, data) {
   let url = getUrl(path)
-  return axios.post(url, data, {
-    headers: {
-      Authorization: window.localStorage.getItem('token')
-    }
-  }).then(proessData)
+  return axios.post(url, data).then(proessData)
 }
 function page (path, currentPage = 1, pageSize = 10, data = {}) {
   data.currentPage = currentPage
